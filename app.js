b85a9816eb44bb56c20f6e150ea2b6015d8cf8cb
Oleksandr <OleksandrW1@gmail.com>
2023-08-29 22:58:46 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// 1
+let day = +prompt("Введіть день тижня");
+
+switch (day) {
+  case 1: alert("Понеділок");
+  break;
+  case 2: alert("Вівторок");
+  break;
+  case 3: alert("Середа");
+  break;
+  case 4: alert("Четвер");
+  break;
+  case 5: alert("П'ятниця");
+  break;
+  case 6: alert("Субота");
+  break;
+  case 7: alert("Неділя");
+  break;
+  default:
+    alert("Невірний день");
+  break;
+}
+
+// 2
+
