f989ac74867f38be2fe782cad21c8ed6b9d4197f
Oleksandr <OleksandrW1@gmail.com>
2023-08-31 23:50:19 +0300

    app.js

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 // 1
-let day = +prompt("Введіть день тижня");
+// let day = +prompt("Введіть день тижня");
 
-switch (day) {
-  case 1: alert("Понеділок");
-  break;
-  case 2: alert("Вівторок");
-  break;
-  case 3: alert("Середа");
-  break;
-  case 4: alert("Четвер");
-  break;
-  case 5: alert("П'ятниця");
-  break;
-  case 6: alert("Субота");
-  break;
-  case 7: alert("Неділя");
-  break;
-  default:
-    alert("Невірний день");
-  break;
-}
+// switch (day) {
+//   case 1: alert("Понеділок");
+//   break;
+//   case 2: alert("Вівторок");
+//   break;
+//   case 3: alert("Середа");
+//   break;
+//   case 4: alert("Четвер");
+//   break;
+//   case 5: alert("П'ятниця");
+//   break;
+//   case 6: alert("Субота");
+//   break;
+//   case 7: alert("Неділя");
+//   break;
+//   default:
+//     alert("Невірний день");
+//   break;
+// }
 
 
 // 2
+
 let a = prompt("введіть число a"); 
 let b = prompt("введіть число b"); 
-let c = a<b; 
-alert(c);
+let c = a>b; 
+
+if(c==true) alert(`Більше ${a}`);
+
+if(c==false) alert(`Менше ${a}`);
+
+
 
 // 3
-
