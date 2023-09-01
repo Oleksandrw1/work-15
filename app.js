c5ea2261892d794e3bd6a5f91be77594dae6284d
Oleksandr <OleksandrW1@gmail.com>
2023-09-01 22:54:43 +0300

    app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // 1
-// let day = +prompt("Введіть день тижня");
+let day = +prompt("Введіть день тижня");
 
-// switch (day) {
-//   case 1: alert("Понеділок");
-//   break;
-//   case 2: alert("Вівторок");
-//   break;
-//   case 3: alert("Середа");
-//   break;
-//   case 4: alert("Четвер");
-//   break;
-//   case 5: alert("П'ятниця");
-//   break;
-//   case 6: alert("Субота");
-//   break;
-//   case 7: alert("Неділя");
-//   break;
-//   default:
-//     alert("Невірний день");
-//   break;
-// }
+switch (day) {
+  case 1: alert("Понеділок");
+  break;
+  case 2: alert("Вівторок");
+  break;
+  case 3: alert("Середа");
+  break;
+  case 4: alert("Четвер");
+  break;
+  case 5: alert("П'ятниця");
+  break;
+  case 6: alert("Субота");
+  break;
+  case 7: alert("Неділя");
+  break;
+  default:
+    alert("Невірний день");
+  break;
+}
 
 
 // 2
@@ -34,43 +34,43 @@
 
 // 2.1
 
-// function min0rMax(a,b,c) {
-//     if (c) {
-//         return a > b ? a : b;
+function min0rMax(a,b,c) {
+    if (c) {
+        return a > b ? a : b;
 
-//     } else {
-//         return a < b ? a : b;
-//     }
-// }
-// console.log(min0rMax(3, 4, false));
+    } else {
+        return a < b ? a : b;
+    }
+}
+console.log(min0rMax(3, 4, false));
 
 // 3
 
-// let costsNumber = promt("Введіть кількість категорій витрат");
-// let myCosts = {
-//     tv: 100,
-//     sport: 200,
-// };
+let costsNumber = promt("Введіть кількість категорій витрат");
+let myCosts = {
+    tv: 100,
+    sport: 200,
+};
 
 
-// for (let i = 1; i <= costsNumber; i++) {
-//     let costsCategory = prompt("Введіть назву категорії");
-//     let costsValue = +prompt("Скільки коштів ви витратили?");
-//     myCosts[costsCategory] = costsValue; 
-// }
-//     const costs = totalCosts(myCosts);
+for (let i = 1; i <= costsNumber; i++) {
+    let costsCategory = prompt("Введіть назву категорії");
+    let costsValue = +prompt("Скільки коштів ви витратили?");
+    myCosts[costsCategory] = costsValue; 
+}
+    const costs = totalCosts(myCosts);
 
-// function totalCosts() {
-//     let total = 0;
+function totalCosts() {
+    let total = 0;
 
-//     for (let key in object) {
-//         total = total + object[key];
-//         // total += object[key];
-//     }
-// return total;
-// }
+    for (let key in object) {
+        total = total + object[key];
+        // total += object[key];
+    }
+return total;
+}
 
-// console.log(`За минулий місяць ви витратили ${costs} грн`);
+console.log(`За минулий місяць ви витратили ${costs} грн`);
 
 
 
